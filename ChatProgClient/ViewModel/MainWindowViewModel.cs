96d057320140628c4eb2a479faf7a5a08a57b498
Daniel Quackenbush <TheQuack45@gmail.com>
2016-11-07 13:49:55 -0500

    Initial commit. Little code yet.

---
+++ ChatProgClient/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatProgClient.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Constructors definition
+        /// <summary>
+        /// Represents the ViewModel for the MainWindow.xaml View.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+
+        }
+        #endregion
+    }
+}
